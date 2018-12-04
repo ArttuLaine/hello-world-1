59fb07734955675b0caee03719760d5893817d3f
ArttuLaine <45590658+ArttuLaine@users.noreply.github.com>
2018-12-04 14:33:14 +0200

    Update helloworld.java

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -3,7 +3,14 @@ package helloworld;
 public class helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("imsiko programming");
+	//Just testing fork functionality
+		String s = "";
+		String sb = "";
+		if (args.length<1) s="Hello World";
+		else s=args[0];
+		sb=new StringBuilder(s).reverse().toString() ;
+
+	System.out.println(s); 
+	System.out.println(sb); 
 	}	
 }
